44a5f599d7544f6efcfaf51116e993c831f05a4f
devmarisales <marisales-jesus@hotmsil.com>
2024-05-02 21:52:32 -0300

    Primeiro Commit - Projeto Mario Bross

---
+++ scripts.js
@@ -0,0 +1,62 @@
+const form = document.querySelector(".formulario-fale-conosco")
+const mascara = document.querySelector(".mascara-formulario")
+
+
+function mostrarForm(){
+    form.style.left ="50%"
+    form.style.transform ="translateX(-50%)"
+    mascara.style.visibilit = "visible"
+}
+
+function esconderForm(){ 
+    form.style.left ="-300px"
+    form.style.transform ="translateX(0)"
+    mascara.style.visibilit = "hidden"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* assistir a aula 08 novamente */
+
+
+
+/*
+
+
+
+/* 
+     " Variaveis - é um espaço que 
+vc reserva e pode guardar
+ o que quiser lá dentro.
+ 
+ 3 jeitos de criar variável:
+ 1- var - "código mt antigo, não se usa mais"
+ 2- let -> "Você consegue alterar o valor depois"
+ 3- const -> Constante - "nâo consegue alterar 
+ o valor  depois"
+ */
+
+/*  
+Funções ->
+    "Um trecho de código 
+    que só é executado quando são chamado"
+*/
+
+
+
+
+
+
+
+
+
